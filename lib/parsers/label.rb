c9932c8f022e60a3be085e97ad7d4adfe14db66e
Seosamh Cahill <seo.cahill@gmail.com>
2017-02-10 16:05:31 +0100

    added label locs. Should now be able to build complete tree

--- lib/parsers/label.rb
+++ lib/parsers/label.rb
@@ -1,20 +1,39 @@
 module LabelParser
+  #
+  # each node has the element_id which corresponds to a label loc
+  # each label loc is linked to a label via a label arc
+  # each node belongs to element
+  # each element has many labels
+  # each label has a standard and a documentation label
+  # each label object should have an element_id == loc.from and a body = loc.to.label
+  #
+
   def parse_label_linkbases
-    entries = { locs: {}, links: {}, resources: {} }
+    labels = []
     Dir.glob("dts_assets/uk-gaap/**/*.xml").grep(/label/) do |file|
       parsed_file = Nokogiri::XML(File.open(file))
-      parsed_file.search('loc', 'labelArc', 'label').each do |item|
-        case item.name
-        when "loc"
-          entries[:locs][item.attributes["label"].value] = hashify_xml(item)
-        when "labelArc"
-          entries[:links][item.attributes["from"].value] = hashify_xml(item)
-        when "label"
-          resource = entries[:resources][item.attributes["label"].value] ||= {}
-          resource[item.attributes["role"].value] = item.text
+      parsed_file.search('labelLink').each do |link|
+        nodes = {}
+        link.search('labelArc', 'label', ).each do |element|
+          if element.name == 'labelArc'
+            nodes[element.attributes['to'].value] = {
+              id: SecureRandom.uuid,
+              element_id: element.attributes['from'].value
+            }
+          else
+            label = nodes[element.attributes['label'].value]
+            label[element.attributes['role'].value.split('/').last] = element.text
+          end
         end
+        labels << nodes.values
       end
     end
-    entries
+    labels.flatten
+  end
+
+  def properties(element)
+    element.elements.each_with_object({}) do |element, object|
+      object[element.name] = element.text
+    end
   end
 end

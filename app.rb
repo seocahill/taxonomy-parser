1ede16019924ee55df45d69c1733a971aa028711
Seosamh Cahill <seo.cahill@gmail.com>
2017-06-29 21:03:08 +0200

    use underscore instead of kebap case routes

--- app.rb
+++ app.rb
@@ -17,17 +17,17 @@ module TaxonomyParser
       super
     end
 
-    get '/discoverable-taxonomy-sets' do
+    get '/discoverable_taxonomy_sets' do
       content_type :json
       $app.discoverable_taxonomy_sets
     end
 
-    get '/discoverable-taxonomy-sets/:id' do
+    get '/discoverable_taxonomy_sets/:id' do
       content_type :json
       $app.discoverable_taxonomy_set(params['id'])
     end
 
-    get '/role-types/:id' do
+    get '/role_types/:id' do
       content_type :json
       $app.role_type(params['id'])
     end
@@ -37,17 +37,17 @@ module TaxonomyParser
       $app.element(params['id'])
     end
 
-    get '/presentation-nodes/:id' do
+    get '/presentation_nodes/:id' do
       content_type :json
       $app.presentation_node(params['id'])
     end
 
-    get '/dimension-nodes/:id' do
+    get '/dimension_nodes/:id' do
       content_type :json
       $app.dimension_node(params['id'])
     end
 
-    get '/elements/:id/dimension-nodes' do
+    get '/elements/:id/dimension_nodes' do
       content_type :json
       $app.element_dimension_nodes(params['id'])
     end

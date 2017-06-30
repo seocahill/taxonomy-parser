603b9e09e27d4cb1415401d92fc1a3c502aafcb9
Seosamh Cahill <seo.cahill@gmail.com>
2017-06-30 19:11:46 +0200

    correct content type

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ require_relative 'lib/application_controller.rb'
 module TaxonomyParser
   class Base < Sinatra::Base
     set :server, :puma
+    mime_type :api_json, 'application/vnd.api+json'
 
     def initialize
       puts "booting app"
@@ -18,52 +19,57 @@ module TaxonomyParser
     end
 
     get '/discoverable_taxonomy_sets' do
-      content_type :json
+      content_type :api_json
       $app.discoverable_taxonomy_sets
     end
 
     get '/discoverable_taxonomy_sets/:id' do
-      content_type :json
+      content_type :api_json
       $app.discoverable_taxonomy_set(params['id'])
     end
 
+    get '/role_types' do
+      content_type :api_json
+      $app.role_types(params)
+    end
+
     get '/role_types/:id' do
-      content_type :json
+      content_type :api_json
       $app.role_type(params['id'])
     end
 
     get '/elements/:id' do
-      content_type :json
+      content_type :api_json
       $app.element(params['id'])
     end
 
     get '/presentation_nodes/:id' do
-      content_type :json
+      content_type :api_json
       $app.presentation_node(params['id'])
     end
 
     get '/dimension_nodes/:id' do
-      content_type :json
+      content_type :api_json
       $app.dimension_node(params['id'])
     end
 
     get '/elements/:id/dimension_nodes' do
-      content_type :json
+      content_type :api_json
       $app.element_dimension_nodes(params['id'])
     end
 
     get '/graph' do
-      content_type :json
+      content_type :api_json
       $app.graph
     end
 
     get '/menu' do
-      content_type :json
+      content_type :api_json
       $app.menu.to_json
     end
 
     get '/dimensions' do
-      content_type :json
+      content_type :api_json
       $app.all_dimensions.to_json
     end
   end

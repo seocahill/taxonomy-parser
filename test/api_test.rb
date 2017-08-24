9712e3c9670e2a337066f581d503098e6b11cd1c
Seosamh Cahill <seo.cahill@gmail.com>
2017-08-24 15:58:39 +0200

    dts test

--- test/api_test.rb
+++ test/api_test.rb
@@ -16,7 +16,14 @@ class APITest < MiniTest::Test
   def test_discoverable_taxonomy_sets
     get '/discoverable_taxonomy_sets'
     assert last_response.ok?, "should be ok"
-    assert_equal 2, jsonapi_data.length, "should return 2 DTS"
-    assert jsonapi_data_includes("name", ["ie-gaap", "uk-gaap"]), "should return correctd DTS data"
+    assert_equal 2, json_data.length, "should return 2 DTS"
+    assert json_includes("name", ["ie-gaap", "uk-gaap"]), "should return correctd DTS data"
+  end
+
+  def test_discoverable_taxonomy_set
+    get '/discoverable_taxonomy_sets/1'
+    assert last_response.ok?, "should be ok"
+    assert_equal json_data["attributes"]["name"], "ie-gaap", "correct DTS"
+    assert_equal 18, json_data("included").length, "should return all presentation role-types"
   end
 end

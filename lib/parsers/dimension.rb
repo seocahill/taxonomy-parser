fd64e802f26e009233574f60b311d296c5886dac
Seosamh Cahill <seo.cahill@gmail.com>
2017-08-25 19:43:04 +0200

    Remove dead code

--- lib/parsers/dimension.rb
+++ lib/parsers/dimension.rb
@@ -1,9 +1,5 @@
 module DimensionParser
 
-  # Items inheriting basic dimensions
-
-  Node = Struct.new(:id, :element_id, :parent_id, :order, :arcrole)
-
   def parse_definition_linkbases
     @def_linkbases = Dir.glob(File.join(__dir__, "/../../dts_assets/#{@current_dts.name}/**/*")).grep(/definition.xml/) do |file|
       parsed_file = Nokogiri::XML(File.open(file))
@@ -27,10 +23,6 @@ module DimensionParser
         end
       end
     end
-  end
-
-  def new_node(element, parent = nil, order = "1", arcrole)
-    Node.new(SecureRandom.uuid, element, parent, order, arcrole)
   end
 
   def dimension_node_tree(element_id)
@@ -81,78 +73,4 @@ module DimensionParser
       [id] + find_domain_members(child_ids)
     end
   end
-
-  def find_primary_items(concept_node)
-    # you need to walk up the full tree not simply a matter of finding the parent and then the parents descendants
-    search_results = {}
-    items = @definitions.flat_map do |parsed_file|
-      find_root_of_tree(parsed_file, concept_node.element_id, 'http://xbrl.org/int/dim/arcrole/domain-member')
-    end
-    nodes = items.flat_map do |item|
-      node = new_node(item.attributes["from"].value, concept_node.id, item.attributes.dig("order")&.value, 'primary-item')
-      find_hypercubes(node)
-    end
-    nodes
-  end
-
-  def find_root_of_tree(parsed_file, current_node, arcrole)
-    current_node_id = current_node.is_a?(String) ? current_node : current_node.attributes["from"].value
-    parents = parsed_file.xpath("//xmlns:definitionArc[@xlink:to='#{current_node_id}' and @xlink:arcrole='#{arcrole}']")
-    if parents.any?
-      parents.flat_map do |parent|
-        find_root_of_tree(parsed_file, parent, arcrole)
-      end
-    else
-      current_node.is_a?(Nokogiri::XML::Element) ? current_node : []
-    end
-  end
-
-  def find_hypercubes(parent)
-    items = @definitions.flat_map do |parsed_file|
-      parsed_file.xpath("//xmlns:definitionArc[@xlink:from='#{parent.element_id}' and @xlink:arcrole='http://xbrl.org/int/dim/arcrole/all']")
-    end.uniq
-    hypercubes = items.flat_map do |item|
-      node = new_node(item.attributes["to"].value, parent.id, item.attributes.dig("order")&.value, item.attributes.dig('arcrole').value)
-      [node.to_h] + find_dimensions(node)
-    end
-    hypercubes
-  end
-
-  def find_dimensions(parent)
-    items = @definitions.flat_map do |parsed_file|
-      parsed_file
-      .xpath("//xmlns:definitionArc[@xlink:from='#{parent.element_id}' and @xlink:arcrole='http://xbrl.org/int/dim/arcrole/hypercube-dimension']")
-    end
-    dimensions = items.flat_map do |item|
-      node = new_node(item.attributes["to"].value, parent.id, item.attributes.dig("order")&.value, item.attributes.dig('arcrole').value)
-      [node.to_h] + find_domains(node)
-    end
-    dimensions
-  end
-
-  def find_domains(parent)
-    arcroles = [
-      "http://xbrl.org/int/dim/arcrole/dimension-default",
-      "http://xbrl.org/int/dim/arcrole/dimension-domain"
-    ]
-    items = @definitions.flat_map do |parsed_file|
-      parsed_file.xpath("//xmlns:definitionArc[@xlink:from='#{parent.element_id}' and contains('#{arcroles}', @xlink:arcrole)]")
-    end
-    domains = items.flat_map do |item|
-      node = new_node(item.attributes["to"].value, parent.id, item.attributes.dig("order")&.value, item.attributes.dig('arcrole').value)
-      [node.to_h] + find_domain_members(node)
-    end
-    domains
-  end
-
-  # def find_domain_members(parent)
-  #   items = @definitions.flat_map do |parsed_file|
-  #     parsed_file.xpath("//xmlns:definitionArc[@xlink:from='#{parent.element_id}' and @xlink:arcrole='http://xbrl.org/int/dim/arcrole/domain-member']")
-  #   end
-  #   domain_members = items.flat_map do |item|
-  #     node = new_node(item.attributes["to"].value, parent.id, item.attributes.dig("order")&.value, item.attributes.dig('arcrole').value)
-  #     [node.to_h] + find_domain_members(node)
-  #   end
-  #   domain_members
-  # end
 end

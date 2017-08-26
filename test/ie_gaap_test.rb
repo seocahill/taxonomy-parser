642bf6127c4c94ef6ed22c416256682b9caad88d
Seosamh Cahill <seo.cahill@gmail.com>
2017-08-26 12:05:20 +0200

    Remove placehodler tests

--- test/ie_gaap_test.rb
+++ test/ie_gaap_test.rb
@@ -7,26 +7,7 @@ class IeGaapTest < MiniTest::Test
   def app
     TaxonomyParser::Base
   end
-
-  # This is important from a UX perspective. If a user selects a tag that has dimensions
-  # without a default, that tag will be invalid unless the user selects a dimension for it.
-  # The API needs to return a boolean indicating whether this is the case to the client.
-  # Dimension nodes should also indictate which hypercube dimension requires the choice.
-  # 
-
-  def test_element_without_all_default_dimensions
-    # uk-direp_FeesDirectors is a deeply nested item that also falls under Entity Officers Hypercube
-    # get primary item
-    # 
-    skip "todo"
-    get '/elements/uk-bus_NameEntityOfficer'
-    assert_equal false, json_data["attributes"]["default-dimensions"], "Entity Officers Dimension has no default value"
-  end
-
-  def test_element_with_all_default_dimensions
-    # uk-gaap_DescriptionDepreciationMethodRateOrUsefulEconomicLifeForTangibleFixedAssets
-    skip "todo"
-  end
-
+  
+  # Taxonomy specific tests
   
 end

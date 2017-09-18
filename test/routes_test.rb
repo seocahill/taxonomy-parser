9885ccab7ce2b7fd873709a245fc12c71d2243c6
Seosamh Cahill <seo.cahill@gmail.com>
2017-09-18 11:42:47 +0200

    adjust tests for fully dasherized routes

--- test/routes_test.rb
+++ test/routes_test.rb
@@ -9,14 +9,14 @@ class DiscoverableTaxonomySetsTest < MiniTest::Test
   end
 
   def test_discoverable_taxonomy_sets
-    get '/discoverable_taxonomy_sets'
+    get '/discoverable-taxonomy-sets'
     assert last_response.ok?, "should be ok"
     assert_equal 2, json_data.length, "should return 2 DTS"
     assert json_includes("name", ["ie-gaap", "uk-gaap"]), "should return correctd DTS data"
   end
 
   def test_discoverable_taxonomy_set
-    get '/discoverable_taxonomy_sets/1'
+    get '/discoverable-taxonomy-sets/1'
     assert last_response.ok?, "should be ok"
     assert_equal json_data["attributes"]["name"], "ie-gaap", "correct DTS"
     assert_equal 18, json_data("included").length, "should return all presentation role-types"
@@ -25,13 +25,13 @@ class DiscoverableTaxonomySetsTest < MiniTest::Test
   def test_role_types
     id = Hash["id", "1,2,3"]
     params = Hash["filter", id]
-    get '/role_types', params
+    get '/role-types', params
     assert last_response.ok?, "should be ok"
     assert_equal 3, json_data.length, "should return 3 Role types"
   end
 
   def test_role_type
-    get '/role_types/1'
+    get '/role-types/1'
     node_names = [
       "Entity information [heading]",
       "Date when ceased to be legal or registered name",
@@ -58,24 +58,24 @@ class DiscoverableTaxonomySetsTest < MiniTest::Test
   def test_presentation_nodes
     id = Hash["id", "100,200,300"]
     params = Hash["filter", id]
-    get '/presentation_nodes', params
+    get '/presentation-nodes', params
     assert last_response.ok?, "should be ok"
     assert_equal 3, json_data.length, "should return 3 Presentation nodes."
   end
 
   def test_presentation_node
-    get '/presentation_nodes/1'
+    get '/presentation-nodes/1'
     assert last_response.ok?, "should be ok"
     assert_equal json_data["attributes"]["name"], "Loans for the purchase of own shares under Section 60 Companies Act 1963", "incorrect name"
   end
 
   def test_dimension_nodes
-    get "/dimension_nodes/1"
+    get "/dimension-nodes/1"
     assert last_response.ok?, "should be ok"
     assert_equal json_data["attributes"]["name"], "Ireland", "incorrect name"
 
     # get node element
-    get "/dimension_nodes/1/element"
+    get "/dimension-nodes/1/element"
     assert last_response.ok?, "should be ok"
     assert_equal json_data["attributes"]["name"], "Ireland", "incorrect name"
   end

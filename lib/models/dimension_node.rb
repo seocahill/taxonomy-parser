23890e020270d52e536c4f0f9e1280ef3e2fe529
Seosamh Cahill <seo.cahill@gmail.com>
2017-10-01 17:35:24 +0200

    Fix default dimension inclusion

--- lib/models/dimension_node.rb
+++ lib/models/dimension_node.rb
@@ -33,6 +33,7 @@ end
 class DimensionNodeSerializer < TaxonomyParser::BaseSerializer
   has_one :element, include_links: false, include_data: true
   has_one :parent, include_links: false, include_data: true
+  has_one :default_dimension, include_links: false, include_data: true
 
-  attributes :order, :name, :tag, :has_defaults, :arcrole, :default_dimension
+  attributes :order, :name, :tag, :has_defaults, :arcrole
 end

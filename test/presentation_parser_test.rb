87e73e581b53b6354bf50c8ef90236c736d23f20
Seosamh Cahill <seo.cahill@gmail.com>
2017-10-03 01:41:11 +0200

    Added  failing test for alias tree issue

--- test/presentation_parser_test.rb
+++ test/presentation_parser_test.rb
@@ -15,8 +15,22 @@ class PresentationParserTest < MiniTest::Test
     # Rather has a series of parent nodes until the root of the subtree is reached whereupon it has two parents
     # "uk-bus_DimensionsParent-EntityContactInfo" and "uk-bus_DimensionsParent-ThirdPartyAgents". 
     # But rather than providing a link from each parent node to the same sub-tree it is preferable to attach
-    # a copy of the subtree to each parent. 
+    # a copy of the subtree to each parent.
+
+    nodes = @test_obj.store[:presentation_nodes].values
     expected = "uk-bus_E-mailAddress"
-    assert_equal 7667, @test_obj.store[:presentation_nodes].size, "Sanity"
+    assert_equal 7667, nodes.size, "All nodes parsed"
+
+    actual_grandparents = lookup_nodes("uk-bus_E-mailAddress").map { |node| node.parent.parent.element.id }.sort
+    expected_grandparents = %w[
+      uk-bus_EntityContactsWebsiteInformationHeading
+      uk-bus_GeneralContactInformationHeading
+      uk-bus_ThirdPartyAgentsHeading
+    ]
+    assert_equal expected_grandparents, actual_grandparents, "presentation tree being constructed correctly"
+  end
+
+  def lookup_nodes(element_id)
+    @test_obj.store[:elements][element_id].presentation_nodes
   end
 end

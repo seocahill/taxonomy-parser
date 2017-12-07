b7f2db3524ee493fa908de87ec8f9c592ea1e645
Seosamh Cahill <seo.cahill@gmail.com>
2017-12-07 23:39:24 +0100

    refactor reference parser

--- lib/parsers/reference_parser.rb
+++ lib/parsers/reference_parser.rb
@@ -4,36 +4,42 @@ module TaxonomyParser
     class << self
 
       def parse(current_dts)
-        reference_id = 0
-        Store.instance.get_data[:references] = {}
-        
+        @id = 1
+        @bucket = Store.instance.get_data[:references] = {}
         Dir.glob(File.join(__dir__, "/../../dts_assets/#{current_dts.name}/**/*.xml")).grep(/reference/) do |file|
-          parsed_file = Nokogiri::XML(File.open(file))
-          parsed_file.search('referenceLink').each do |link|
-            nodes = {}
+          linkbase = Nokogiri::XML(File.open(file))
+          process_linkbase(linkbase)
+        end
+      end
 
-            link.search('referenceArc', 'reference', ).each do |node|
-              if node.name == 'referenceArc'
-                element = Store.instance.get_data[:elements][node.attributes['from'].value]
-                reference_id += 1
-                reference = Reference.new(reference_id, element)
-                element.reference = reference
-                nodes[node.attributes['to'].value] = reference
-              else
-                reference = nodes[node.attributes['label'].value]
-                reference.reference_data = node.elements.map do |el|
-                  Hash[el.name, el.text]
-                end
-              end
-            end
-
-            nodes.values.each do |node|
-              Store.instance.get_data[:references][node.id] = node
+      def process_linkbase(parsed_file)
+        parsed_file.search('referenceLink').each do |link|
+          nodes = {}
+          link.search('referenceArc', 'reference').each do |node|
+            if node.name == 'referenceArc'
+              nodes[node.attributes['to'].value] = add_new_reference(node)
+            else
+              reference = nodes[node.attributes['label'].value]
+              update_reference(reference, node)
             end
           end
         end
       end
 
+      def add_new_reference(node)
+        element = Store.instance.get_data[:elements][node.attributes['from'].value]
+        reference = Reference.new(@id, element)
+        element.reference = reference
+        @bucket[@id] = reference
+        @id += 1
+        reference
+      end
+
+      def update_reference(reference, node)
+        reference.reference_data = node.elements.map do |el|
+          Hash[el.name, el.text]
+        end
+      end
     end
   end
 end
